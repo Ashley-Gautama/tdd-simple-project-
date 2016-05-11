00aab5bb664418fa9f8ae43a19745cb51ab1e800
Ashley-Gautama <AshleyGautama.com>
2016-05-11 23:28:16 +0200

    added actor index and show action

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,2 +1,2 @@
-class ActorController < ApplicationController
+class ActorsController < ApplicationController
 end
--- spec/controllers/actor_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ActorController, type: :controller do
-
-end
