0c973e9ce55788f5b3d9e33e689ee92c1e8aa6f9
Ashley-Gautama <AshleyGautama.com>
2016-05-11 14:02:41 +0200

    generated models and controllers

---
+++ spec/factories/actors.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :actor do
+    name "MyString"
+    bio "MyText"
+    age 1
+  end
+end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :movie do
+    title "MyString"
+    description "MyText"
+    releasedate 1
+  end
+end
