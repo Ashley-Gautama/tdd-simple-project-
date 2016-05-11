710120a66ae882ef044c254f854f988b13906874
Ashley-Gautama <AshleyGautama.com>
2016-05-11 16:36:31 +0200

    added test for model Movie

--- spec/factories/movies.rb
+++ spec/factories/movies.rb
@@ -1,7 +1,9 @@
+
+require 'faker'
 FactoryGirl.define do
-  factory :movie do
-    title "MyString"
-    description "MyText"
-    releasedate 1
+  factory :movie do |f|
+    f.title Faker::Book.title
+    f.description Faker::Lorem.sentences
+    f.releasedate Faker::Number.number(4)
   end
 end
